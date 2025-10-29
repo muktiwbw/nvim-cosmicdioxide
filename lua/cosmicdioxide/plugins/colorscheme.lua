1d5174a9035fc2b7e827c2aa5b39033bd40ff133
Mukti WIbowo Aji Joyonegoro <mukti.joyonegoro@ninjavan.co>
2025-10-29 16:37:56 +0700

    Added gruvbox-material colorscheme

--- lua/cosmicdioxide/plugins/colorscheme.lua
+++ lua/cosmicdioxide/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
   {
-    'sainnhe/everforest',
+    'sainnhe/gruvbox-material',
     lazy = false,
     priority = 1,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_foreground = 'original'
+      vim.cmd.colorscheme('gruvbox-material')
+    end
+  },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 2,
     config = function()
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
